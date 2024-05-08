70c8811385c40c70f2c7634f9777600efb9d9eea
Josephine mwende <josymwesh45@gmail.com>
2024-05-08 06:48:39 -0700

    puppet manifest for installing flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
 # install flask from pip3
 exec { 'flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+  command     => '/usr/bin/pip3 install flask==2.1.0',
+  refreshonly => 'true',
+  unless      => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
